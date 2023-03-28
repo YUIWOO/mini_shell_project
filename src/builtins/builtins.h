7548f3da36aa7d59366cc8cc91c8ca46634857b2
Youngwoo Cho <youngwch@c5r2s6.42seoul.kr>
2023-03-28 16:25:49 +0900

    [FIX]fix

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yuikim <yuikim@student.42.fr>              +#+  +:+       +#+        */
+/*   By: youngwch <youngwch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 11:38:07 by yuikim            #+#    #+#             */
-/*   Updated: 2023/03/28 10:49:51 by yuikim           ###   ########.fr       */
+/*   Updated: 2023/03/28 15:23:21 by youngwch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int		pwd(void);
 int		cd(char *path, char ***envp);
 char	*get_pwd(void);
 int		export(char *arg, char ***envp);
-int		unset(char *key, char **envp);
+int	unset(char *key, char **envp);
 int		echo(char **arg);
 void	ms_exit(char **arg, int exit_code);
 int		export_no_option(char **envp);
@@ -45,6 +45,7 @@ void	append_dptr(char ***dptr, char *statement);
 void	set_env_statement(char **env_addr, char *key, char *value);
 void	make_env_statement(char ***envp_ptr, char *key, char *value);
 void	ft_sort_ascii(char **str_arr, int size);
+int	check_builtins(char **args, char ***envp);
 
 //type
 typedef struct s_info{
--- src/builtins/check_builtins.c
+++ src/builtins/check_builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_builtins.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yuikim <yuikim@student.42.fr>              +#+  +:+       +#+        */
+/*   By: youngwch <youngwch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 20:09:00 by yuikim            #+#    #+#             */
-/*   Updated: 2023/03/28 10:54:35 by yuikim           ###   ########.fr       */
+/*   Updated: 2023/03/28 16:23:41 by youngwch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,29 @@ static int	execute_arg_size_zero(char **args, char ***envp)
 		return (export_no_option(*envp));
 	if (ft_strncmp(args[0], "unset", ft_strlen("unset")) == 0)
 		return (0);
+	return (-1);
 }
 
 int	check_builtins(char **args, char ***envp)
 {
 	if (!args)
 		return (-1);
+	if (get_dptr_size(args) == 0)
+		return (0);
 	if (ft_strncmp(args[0], "echo", ft_strlen("echo")) == 0)
 		return (echo(args));
 	if (ft_strncmp(args[0], "pwd", ft_strlen("pwd")) == 0)
 		return (pwd());
-	if (get_dptr_size(args) == 0)
-		return (execute_arg_size_zero(args, envp));
 	if (get_dptr_size(args) == 1)
+		return (execute_arg_size_zero(args, envp));
+	if (get_dptr_size(args) == 2)
 	{
 		if (ft_strncmp(args[0], "cd", ft_strlen("cd")) == 0)
-			return (cd(args[0], envp));
+			return (cd(args[1], envp));
 		if (ft_strncmp(args[0], "export", ft_strlen("export")) == 0)
-			return (export(args[0], envp));
+			return (export(args[1], envp));
 		if (ft_strncmp(args[0], "unset", ft_strlen("unset")) == 0)
-			return (unset(args[0], envp));
+			return (unset(args[1], *envp));
 		if (ft_strncmp(args[0], "exit", ft_strlen("exit")) == 0)
 			ms_exit(args, ft_atoi(args[1]) % 256);
 	}
