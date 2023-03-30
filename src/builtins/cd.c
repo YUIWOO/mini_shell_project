b97fc31bcccb18fecdf1afcdc7ed99127b33ab4c
Yuijin Kim <yuikim@c5r4s5.42seoul.kr>
2023-03-30 19:26:43 +0900

    [fix] modify cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: youngwch <youngwch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yuikim <yuikim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 13:29:05 by yuikim            #+#    #+#             */
-/*   Updated: 2023/03/28 15:38:44 by youngwch         ###   ########.fr       */
+/*   Updated: 2023/03/30 19:24:54 by yuikim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,7 @@ int	cd(char *path, char ***envp)
 	set_env_value(envp, "OLDPWD", buffer);
 	free(buffer);
 	path_info = ft_split(path, '/');
-	if (ft_strncmp(path_info[0], "~",
-			ft_strlen(path_info[0])) == 0)
-	{
-		free(path_info[0]);
-		path_info[0] = get_env_value(*envp, "HOME");
-		new_path = str_total_join(path_info, "/");
-		free_dptr(path_info, HOME);
-	}
-	else
-	{
-		new_path = str_total_join(path_info, "/");
-		free_dptr(path_info, DEFAULT);
-	}
+	new_path = str_total_join(path_info, "/");
+	free_dptr(path_info, DEFAULT);
 	return (change_pwd_env(new_path, envp));
 }
