24b5908cc5d6e0f9b16c1c6491bf79baee22b9dd
Yuijin Kim <yuikim@c5r3s5.42seoul.kr>
2023-03-31 11:41:46 +0900

    [fix] modify cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,16 +6,15 @@ static int	change_pwd_env(char *new_path, char ***envp)
 
 	if (chdir(new_path) == -1)
 	{
-		free(new_path);
 		show_error("cd");
 		return (1);
 	}
 	buffer = get_pwd();
 	set_env_value(envp, "PWD", buffer);
-	free(new_path);
 	free(buffer);
 	return (0);
 }
+
 
 int	cd(char *path, char ***envp)
 {
@@ -23,12 +22,8 @@ int	cd(char *path, char ***envp)
 	char	*new_path;
 	char	*buffer;
 
-	printf("here %s\n", path);
 	buffer = get_pwd();
 	set_env_value(envp, "OLDPWD", buffer);
 	free(buffer);
-	path_info = ft_split(path, '/');
-	new_path = str_total_join(path_info, "/");
-	free_dptr(path_info, DEFAULT);
-	return (change_pwd_env(new_path, envp));
+	return (change_pwd_env(path, envp));
 }
